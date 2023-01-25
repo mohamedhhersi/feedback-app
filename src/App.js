3bb7f2d59d073e4e8a21e3709474bd2adfeb28d1
Mohamed <mohamed.hersi1232@gmail.com>
2023-01-25 06:26:40 -0500

    added css styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from './components/Header'
+
 function App() {
     return (
-        <h1>Hello from the app component</h1>
+        <>
+        <Header />
+        <h1>My App</h1>
+        </>
     )
+
 }
 
 export default App
