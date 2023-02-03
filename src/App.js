c715fd8d8e61876111b6bb2abd108020c456f2b6
Mohamed Hersi <mohamed.mohamed.hersi1232@gmail.com>
2023-02-03 02:03:31 -0500

    added feedbacklist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header />
-      <FeedbackItem />
+      <FeedbackList feedback = {feedback} />
     </>
   );
 }
