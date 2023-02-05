ef60b48f2b0d9ba9f0925666ec6e183b6b3e8237
Mohamed Hersi <mohamed.mohamed.hersi1232@gmail.com>
2023-02-05 04:42:33 -0500

    feedbackitem

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 
 function FeedbackList({feedback}) {
@@ -7,10 +8,23 @@ function FeedbackList({feedback}) {
   return (
     <div className='feedback-list'>
     {feedback.map((item)=>{
-        return <FeedbackItem key ={item.id} item = {item} />
+        return <FeedbackItem 
+        key ={item.id} 
+        item = {item} 
+        handleDelete={(id)=> console.log(id)}
+        />
 })}
     </div>
   )
 }
 
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+}
 export default FeedbackList
