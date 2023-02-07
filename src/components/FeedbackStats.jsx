9fe00f840c7651c2eebdc09437942d1d4455861b
Mohamed <mohamed.hersi1232@gmail.com>
2023-02-07 06:06:06 -0500

    created feedbackform and feedback button

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -6,7 +6,6 @@ export default function FeedbackStats({feedback}) {
 let average = feedback.reduce((acc, cur)=>{
     return acc + cur.rating
 }, 0) / feedback.length
-console.log(average)
 
 average = average.toFixed(1).replace(/[.,]0$/,'')
   return (
