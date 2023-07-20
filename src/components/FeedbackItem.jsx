1d0f22e81a719a33acbb56fbf042214b9f4c4292
Mohamed Hersi <mohamed.hersi1232@gmail.com>
2023-07-20 09:11:35 -0400

    fixed errors

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,21 @@
-import {FaTimes, FaEdit} from 'react-icons/fa'
-import { useContext } from 'react';
-import PropTypes from 'prop-types'
+import { FaTimes, FaEdit } from "react-icons/fa";
+import { useContext } from "react";
+import PropTypes from "prop-types";
 import Card from "./shared/Card";
-import { useState } from "react";
-import FeedbackContext from '../context/FeedbackContext';
+// import { useState } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
 
-function FeedbackItem({item}) {
-  const {deleteFeedback, editFeedback} = useContext(FeedbackContext) 
-  
   return (
-    <Card >
+    <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={()=> deleteFeedback(item.id)} className="close">
-        <FaTimes color ='purple' />
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color="purple" />
       </button>
-      <button onClick={()=> editFeedback(item)} className="edit">
-        <FaEdit color= 'purple'/>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
@@ -24,9 +23,7 @@ function FeedbackItem({item}) {
 }
 
 FeedbackItem.propTypes = {
-  item: PropTypes.object.isRequired
-}
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
-
-
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 function Header({ text, bgColor, textColor }) {
   const headerStyles = {
@@ -19,7 +19,6 @@ Header.defaultProps = {
   text: "Feedback UI",
   bgColor: "rgba(0,0,0,0.4)",
   textColor: "#ff6a95",
-  
 };
 
 export default Header;
